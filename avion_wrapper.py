3824e90e0ffb84bff6f071060a48c93aebc85995
eongan <eongan@ethz.ch>
2025-06-01 19:46:38 +0200

    add stuff

--- avion_wrapper.py
+++ avion_wrapper.py
@@ -4,10 +4,69 @@ import torch
 import torch.nn.functional as F
 import avion.models.model_clip as model_clip
 from avion.models.utils import inflate_positional_embeds
-from avion.utils.misc import generate_label_map
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
+import csv
+
+
+def generate_label_map(dataset):
+    if dataset == 'ek100_cls':
+        print("Preprocess ek100 action label space")
+        vn_list = []
+        mapping_vn2narration = {}
+        for f in [
+            'conf/ek100/EPIC_100_train.csv',
+            'conf/ek100/EPIC_100_validation.csv',
+        ]:
+            csv_reader = csv.reader(open(f))
+            _ = next(csv_reader)  # skip the header
+            for row in csv_reader:
+                vn = '{}:{}'.format(int(row[10]), int(row[12]))
+                narration = row[8]
+                if vn not in vn_list:
+                    vn_list.append(vn)
+                if vn not in mapping_vn2narration:
+                    mapping_vn2narration[vn] = [narration]
+                else:
+                    mapping_vn2narration[vn].append(narration)
+                # mapping_vn2narration[vn] = [narration]
+        vn_list = sorted(vn_list)
+        print('# of action= {}'.format(len(vn_list)))
+        mapping_vn2act = {vn: i for i, vn in enumerate(vn_list)}
+        print('mapping_vn2act', mapping_vn2act)
+        labels = [list(set(mapping_vn2narration[vn_list[i]])) for i in range(len(mapping_vn2act))]
+        # shape of the labels
+        # print(len(labels), len(labels[0]), labels[0])
+        # print(labels[:5])
+    elif dataset == 'charades_ego':
+        print("=> preprocessing charades_ego action label space")
+        vn_list = []
+        labels = []
+        with open('datasets/CharadesEgo/CharadesEgo/Charades_v1_classes.txt') as f:
+            csv_reader = csv.reader(f)
+            for row in csv_reader:
+                vn = row[0][:4]
+                vn_list.append(vn)
+                narration = row[0][5:]
+                labels.append(narration)
+        mapping_vn2act = {vn: i for i, vn in enumerate(vn_list)}
+        print(labels[:5])
+    elif dataset == 'egtea':
+        print("=> preprocessing egtea action label space")
+        labels = []
+        with open('datasets/EGTEA/action_idx.txt') as f:
+            for row in f:
+                row = row.strip()
+                narration = ' '.join(row.split(' ')[:-1])
+                labels.append(narration.replace('_', ' ').lower())
+                # labels.append(narration)
+        mapping_vn2act = {label: i for i, label in enumerate(labels)}
+        print(len(labels), labels[:5])
+    else:
+        raise NotImplementedError
+    return labels, mapping_vn2act
 
 
 class AVIONForwardModule(nn.Module):
@@ -28,11 +87,19 @@ class AVIONForwardModule(nn.Module):
         self.register_buffer("mean", mean)
         self.register_buffer("std",  std)
 
+        # Read class mapping CSV files
+        self.noun_to_noun_text = pd.read_csv('conf/ek100/EPIC_100_noun_classes.csv')
+        self.verb_to_verb_text = pd.read_csv('conf/ek100/EPIC_100_verb_classes.csv')
+
+        label, mapping_vn2act = generate_label_map('ek100_cls')
+        self.mapping_act2v = {i: int(vn.split(':')[0]) for (vn, i) in mapping_vn2act.items()}
+        self.mapping_act2n = {i: int(vn.split(':')[1]) for (vn, i) in mapping_vn2act.items()}
+
 
     """
     initializes the backbone model
     """
-    def initialize_backbone(self, pretrain_path="avion_pretrain_lavila_vitb_best.pt", fine_tune_path="avion_finetune_cls_lavila_vitb_best.pt"):
+    def initialize_backbone(self, pretrain_path="conf/checkpoints/avion/avion_pretrain_lavila_vitb_best.pt", fine_tune_path="conf/checkpoints/avion/avion_finetune_cls_lavila_vitb_best.pt"):
 
         ckpt = torch.load(pretrain_path, map_location='cpu')
         state_dict = OrderedDict()
@@ -128,7 +195,7 @@ class AVIONForwardModule(nn.Module):
         frames = (frames - mean) / std
 
         # 5. TCHW → CTHW, add batch dim, move to CUDA
-        frames = frames.permute(1, 0, 2, 3).unsqueeze(0).to("cuda")
+        frames = frames.permute(1, 0, 2, 3).unsqueeze(0).to("cuda").to(torch.bfloat16)
 
         # 6. imitate the autocast in your update() loop:
         #    keep the graph JIT-safe by *explicitly* casting to BF16
@@ -139,3 +206,11 @@ class AVIONForwardModule(nn.Module):
         # logits are BF16 – do softmax in FP32 for numerical stability
         probs = torch.softmax(logits.float(), dim=1)
         return logits.argmax(1), probs
+    
+    def get_text(self, pred):
+        n_idx = self.mapping_act2n[pred]
+        v_idx = self.mapping_act2v[pred]
+        n_text = self.noun_to_noun_text['key'][n_idx]
+        v_text = self.verb_to_verb_text['key'][v_idx]
+
+        return v_text, n_text
