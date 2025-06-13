91469f732ea688bb812e891e0639f92fbb787921
eongan <eongan@ethz.ch>
2025-06-13 14:07:16 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="avion",
     version="1.0",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     author="someone",
     description=["AVION"],
     url="git@github.com:3dvision-cecn/AVION_BUT.git",
